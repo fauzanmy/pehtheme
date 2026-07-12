b01e25ac41c7bf3cd9e0bd758035d2e34e074885
Fauzan My <bangpozan@gmail.com>
2026-07-12 12:50:43 +0700

    feat: update toggle component import to use the latest version

--- src/assets/js/app-module.js
+++ src/assets/js/app-module.js
@@ -3,5 +3,5 @@ import { domReady } from './utils/dom-ready.js';
 domReady(async () => {
 	//await import('./features/dark-mode.js');
 	//await import('./features/shadow-effect.js');
-	await import('./components/toggle-v8.js');
+	await import('./components/toggle.js');
 });
