f422d2ce8fb3a4c7e09af11d6114a566fcc31d7c
Fauzan My <bangpozan@gmail.com>
2026-07-17 11:44:00 +0700

    refactor: comment out passthrough copy for icons in Eleventy configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 export default function (eleventyConfig) {
 	eleventyConfig.addWatchTarget('./src/assets/css/');
 	eleventyConfig.addPassthroughCopy({ 'src/assets/images': 'assets/images' });
-	eleventyConfig.addPassthroughCopy({ 'src/assets/icons': 'assets/icons' });
+	// eleventyConfig.addPassthroughCopy({ 'src/assets/icons': 'assets/icons' });
 
 	return {
 		dir: {
